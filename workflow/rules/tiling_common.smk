627e46fc67de669f0c9ec14e2da933dd4e86d7b2
Max <max.schubach@bihealth.de>
2022-11-29 17:30:10 +0100

    feat: tiling implementation

--- workflow/rules/tiling_common.smk
+++ workflow/rules/tiling_common.smk
@@ -1,6 +1,22 @@
 def tiling_getInputFile(sample):
     """Return the input file for the given sample where tiling should be used."""
     if config["tiling"]["remove_edge_variants"]:
-        return samples[sample]["bed_file"]
+        return samples.loc[sample]["bed_file"]
     else:
         return "results/tiling/%s/regions/extended.bed.gz" % sample
+
+
+def tiling_getMinTwoTiles():
+    variant_edge_size = (
+        config["tiling"]["variant_edge_exclusion"] * 2
+        if config["tiling"]["strategies"]["two_tiles"]["include_variant_edge"]
+        else 0
+    )
+    return min(
+        [
+            config["tiling"]["strategies"]["two_tiles"]["max"],
+            2 * config["oligo_length"]
+            - 2 * config["tiling"]["min_overlap"]
+            - variant_edge_size,
+        ]
+    )
--- workflow/scripts/tiling/centerTiling.py
+++ workflow/scripts/tiling/centerTiling.py
@@ -1,5 +1,6 @@
 import click
-
+import gzip
+import copy as cp
 # options
 
 
@@ -26,15 +27,43 @@ import click
               help='Output bed file')
 def cli(input_file, oligo_length, min_overlap, output_file):
 
-    for line in open(input_file):
-        chrom, start, end = line.strip().split('\t')
+    if is_gz_file(input_file):
+        file = gzip.open(input_file, 'rt')
+    else:
+        file = open(input_file, 'r')
+
+    output = open(output_file, "w")
+    for line in file:
+        
+        line_split = line.strip().split('\t')
+        start, end = line_split[1:3]
         start, end = int(start), int(end)
-        for tile in tileRegion(start, end, oligo_length, min_overlap):
-            print(chrom, tile[0], tile[1], sep='\t', file=open(output_file, 'a'))
+        tiles = [tile for tile in tileCenterRegion(start, end, oligo_length, min_overlap)]
+        n_tiles=len(tiles)
+        n_tiles_half=int(n_tiles/2)
+
+        for i, tile in enumerate(tiles):
+            print_line = cp.deepcopy(line_split)
+            print_line[1] = str(tile[0])
+            print_line[2] = str(tile[1])
+
+            # check for correct order. center must be in the center
+            if i == 0:
+                print_line[3] =  '%s_tile%d-%d' % (print_line[3], n_tiles_half+1, n_tiles)
+            elif (i> n_tiles_half):
+                print_line[3] =  '%s_tile%d-%d' % (print_line[3], i+1, n_tiles)
+            else:
+                print_line[3] =  '%s_tile%d-%d' % (print_line[3], i, n_tiles)
+            
+            output.write('\t'.join(print_line) + '\n')
+    output.close()
+
+def is_gz_file(filepath):
+    with open(filepath, 'rb') as test_f:
+        return test_f.read(2) == b'\x1f\x8b'
 
 
-
-def tileRegion(start, end, oligo_length, min_overlap):
+def tileCenterRegion(start, end, oligo_length, min_overlap):
     """Tiling function for a single region.
 
     Parameters
@@ -58,17 +87,39 @@ def tileRegion(start, end, oligo_length, min_overlap):
     yield (center_left, center_right)
 
     # left
-    left_end = center_left + min_overlap
+    for tile_left, tile_right in tileRegion(start, center_right, oligo_length, min_overlap, isLeft=True):
+        yield (tile_left, tile_right)
+    for tile_left, tile_right in tileRegion(center_left, end, oligo_length, min_overlap, isLeft=False):
+        yield (tile_left, tile_right)
 
-    div = (left_end - start) // (oligo_length-min_overlap/2)
-    if (left_end - start) % (oligo_length-min_overlap/2):
+
+def tileRegion(start, end, oligo_length, min_overlap, isLeft=True):
+
+    div = ((end - start) - min_overlap) // (oligo_length-min_overlap)
+    if ((end - start) - min_overlap) % (oligo_length-min_overlap) != 0:
         div += 1
-    
 
-    for i in range(start, end, oligo_length):
-        yield (i, i + oligo_length)
+    # just center, left, right
+    if div == 2:
+        if isLeft:
+            yield(start, start+oligo_length)
+        else:
+            yield(end-oligo_length, end)
 
-
+    else:
+        extension = (((oligo_length-min_overlap) - ((end - start) - min_overlap) %
+                     (oligo_length-min_overlap))) % (oligo_length-min_overlap) // (div-1)
+        first = ((oligo_length-min_overlap) - ((end - start) - min_overlap) % (oligo_length-min_overlap)) % (div-1)
+        for i in range(div):
+            tile_left = start
+            tile_right = start + oligo_length
+            # don't get center
+            if not ((i == div-1 and isLeft) or (i == 0 and not isLeft)):
+                yield(tile_left, tile_right)
+            if (i == div-2 and isLeft) or (i == 0 and not isLeft):
+                start = tile_right-(extension+min_overlap+first)
+            else:
+                start = tile_right-(extension+min_overlap)
 
 
 if __name__ == '__main__':
