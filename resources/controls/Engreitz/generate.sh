cefafc71c8ac918115f9977e021c19f35ea6fd81
Max Schubach <max.schubach@bih-charite.de>
2023-02-01 14:16:19 +0100

    Engreitz controls (all overlapping TSS)

--- resources/controls/Engreitz/generate.sh
+++ resources/controls/Engreitz/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat original_regions.bed | sort -k 1,1 -k2,2n | \
-bedtools slop -s -l 7 -r -1 -g /fast/projects/cubit/current/static_data/reference/GRCh38/hs38/hs38.fa.genome -i - | 
+bedtools slop -s -l 6 -r 0 -g /fast/projects/cubit/current/static_data/reference/GRCh38/hs38/hs38.fa.genome -i - | 
 sort -k 1,1 -k2,2n | uniq | \
 bgzip -c > Engreitz.housekeeping.promoters.controls.bed.gz
 
