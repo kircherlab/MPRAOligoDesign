37858fa85f850b4238a32b7d80f213844af6f06b
Max <max.schubach@bihealth.de>
2023-02-17 12:22:04 +0100

    genearte controls fix

--- resources/controls/Selvarajan/generate.sh
+++ resources/controls/Selvarajan/generate.sh
@@ -3,7 +3,6 @@
     cat Selvarajan.HepG2.STARR-seq.shifted.controls.fasta | egrep ">" | sed 's/>//g' | \
         sed 's/_/\t/g' | awk -v "OFS=\t" '{print $1,$2-1-10,$3+10,$9"|"$10,".","+"}';
 ) | sort -k 1,1 -k2,2n | uniq | \
-bedtools merge -s -c 4,5,6 -o distinct -d -1 -i - | \
 bgzip -c > Selvarajan.HepG2.STARR-seq.shifted.controls.bed.gz
 
 (cat << EOF
